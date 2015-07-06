cf088986e6d4499f52e603e93802b827b0e66fa1
gbelot2003 <gbelot2003@hotmail.com>
2015-07-06 05:09:48 -0600

    PageController, added guest middleware

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -9,6 +9,16 @@ use App\Http\Controllers\Controller;
 
 class PagesController extends Controller
 {
+	/**
+	 * Create a new controller instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		$this->middleware('guest');
+	}
+
     public function index()
 	{
 		return View('welcome');
