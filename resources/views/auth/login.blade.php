9647bc5ab0fadba888b66cc2dfa6a9d34a23fff1
gbelot2003 <gbelot2003@hotmail.com>
2015-07-06 05:42:06 -0600

    auth.login enacement of inputfields

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,17 +7,22 @@
 			<p class="flow-text">Login</p>
 
 			<div class="row">
-				<div class="col s12">
-					<input id="email" name="email" type="email" class="validate" placeholder="E-mail">
+				<div class="input-field col s12">
+					<i class="material-icons prefix white-text">account_circle</i>
+					<input id="email" name="email" type="email" class="validate">
+					<label class="white-text" for="email">Correo Electronico</label>
 				</div>
 			</div>
 			<div class="row">
-				<div class="col s12">
-					<input id="password" type="password" name="password" class="validate" placeholder="Contraseña">
+				<div class=" input-field col s12">
+					<i class="material-icons prefix white-text">vpn_key</i>
+					<input id="password" type="password" name="password" class="validate" minlength="6" length="15">
+					<label class="white-text" for="password">Contraseña</label>
 				</div>
 			</div>
-
-			<P><input type="submit" class="btn white black-text" value="Enviar"/></P>
+			<div class="row">
+				<input type="submit" class="btn white black-text text-center" value="Enviar"/>
+			</div>
 		</form>
 	</div>
 </div>
