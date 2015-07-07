6b447dcf7548a5c9108369ed8859e93d6014c667
gbelot2003 <gbelot2003@hotmail.com>
2015-07-06 20:47:45 -0600

    Gulp, Adding jqueryDatatable librarie

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,7 @@ elixir(function(mix) {
     /** JavaScript jquery dependensie **/
     mix.copy('vendor/bower_components/jquery/dist/jquery.js', 'resources/assets/js/jquery.js');
     mix.copy('vendor/bower_components/materialize/dist/js/materialize.js', 'resources/assets/js/materialize.js');
+    mix.copy('vendor/bower_components/datatables/media/js/jquery.dataTables.js', 'resources/assets/js/dataTable.js');
     mix.copy('vendor/bower_components/moment/min/moment.min.js', 'resources/assets/js/moment.js');
     mix.copy('vendor/bower_components/fullcalendar/dist/fullcalendar.js', 'resources/assets/js/fullcalendar.js');
 
@@ -24,12 +25,20 @@ elixir(function(mix) {
 
     /** sass dependensies **/
     mix.copy('vendor/bower_components/fullcalendar/dist/fullcalendar.css', 'resources/assets/sass/fullcalendar.scss');
+    mix.copy('vendor/bower_components/datatables/media/css/jquery.dataTables.css', 'resources/assets/sass/dataTable.scss');
 
 });
 
 elixir(function(mix) {
     /** jquery and libraries **/
-    mix.scripts(['jquery.js', 'materialize.js', 'moment.js', 'fullcalendar.js', 'init.js'], 'public/js/app.js');
+    mix.scripts([
+        'jquery.js',
+        'materialize.js',
+        'moment.js',
+        'fullcalendar.js',
+        'dataTable.js',
+        'init.js'
+        ], 'public/js/app.js');
 
     /** vue scripts **/
     mix.scripts(['vue.js', 'vue-resource.js', 'vue-permisos.js'], 'public/js/vue-permisos.js')
