bc5b89ccf672ff1a2565f78c4676247102670954
gbelot2003 <gbelot2003@hotmail.com>
2015-07-07 20:01:57 -0600

    register PermissionCheckPerms on kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -28,6 +28,7 @@ class Kernel extends HttpKernel
     protected $routeMiddleware = [
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+		'PermissionCheck' => \App\Http\Middleware\PermissionCheckPerms::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
     ];
 }
