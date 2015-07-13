6beb52bfae36f3bf21b4b1e393d77809977c87bc
gbelot2003 <gbelot2003@hotmail.com>
2015-07-13 04:40:52 -0600

    UsersTableSeeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -22,13 +22,24 @@ class UsersTableSeeder extends Seeder {
 				'name' => 'Gerardo Belot',
 				'email' => 'gbelot2003@hotmail.com',
 				'password' => '$2y$10$OpcKFzqU1hgb53IIKvkKUu3yj9T7OQw5gbPqEN/CjNAGagL5NNUEq',
-				'remember_token' => 'wqkKOjK5LEDZnmlBqZWKkSTEqcNlHhEvXNWB77ekdH018tATdkDAsrPE8MsP',
+				'remember_token' => '8J9zbAuYZ1rOHfrCi4kPAoJSbXzDGXo22bHi3x4Y5ez4C9qMlNSeud3bZybV',
 				'created_at' => '2015-07-06 09:46:45',
-				'updated_at' => '2015-07-06 11:29:18',
+				'updated_at' => '2015-07-13 10:31:14',
+			],
+			1 => 
+			[
+				'id' => '2',
+				'userstatus_id' => '1',
+				'name' => 'Luis Alberto Tejada',
+				'email' => 'tejada@yahoo.com',
+				'password' => '$2y$10$I/pzFWJj/NbDdivQBVyte.wc3IX24rIoOyxfL3Rkh5Z8MWarx8agy',
+				'remember_token' => NULL,
+				'created_at' => '2015-07-13 09:39:39',
+				'updated_at' => '2015-07-13 10:33:43',
 			],
 		]);
-
 		$user = User::find(1); $user->roles()->attach(2);
+		$user = User::find(2); $user->roles()->attach(11);
 	}
 
 }
