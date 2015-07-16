6bf014f5738e33309684809839447c63d7e6ce93
gbelot2003 <gbelot2003@hotmail.com>
2015-07-16 12:01:37 -0600

    nav agregando registros de casos

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -5,6 +5,7 @@
 		@if (Auth::guest())
 			<li><a href="auth/login"><i class="material-icons right">lock</i>Login</a></li>
 		@else
+			<li><a href="/casos">Registros de casos</a></li>
 			<li class="no-padding">
 				<ul class="collapsible collapsible-accordion">
 					<li>
@@ -57,6 +58,7 @@
 			<li><a href="auth/login"><i class="material-icons right">lock</i>Login</a></li>
 
 		@else
+			<li><a href="/casos">Registros de casos</a></li>
 			<li><a class="dropdown-button" href="#!" data-activates="dropdown2">Contactos<i class="mdi-navigation-arrow-drop-down right"></i></a></li>
 			<ul id='dropdown2' class='dropdown-content'>
 				<li><a href="{{ url('clientes') }}">Clientes</a></li>
