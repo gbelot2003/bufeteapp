223b5ec6edda012470e6c0025d2c6ec3c3211f35
gbelot2003 <gbelot2003@hotmail.com>
2015-08-16 16:25:28 -0600

    dash._create-modal adding markup for datetimepicker implementation

--- resources/views/dash/_create-modal.blade.php
+++ resources/views/dash/_create-modal.blade.php
@@ -4,14 +4,14 @@
 
 		<div class="modal-content">
 
-			<h4>Nuevo Evento de calendario</h4>
+			<h4>Crear evento</h4>
 
 			<div class="col s12 amber lighten-5 diverblock">
 				<span><strong>Inicio</strong></span>
 
 				<div class="row">
 
-					<div class="s12 col">
+					<div class="s12 m4 col">
 
 						<div class="input-field">
 							<input type="text" name="title" id="inputTitle" v-model="title"/>
@@ -20,15 +20,17 @@
 
 					</div>
 
-					<div class="s12 m4 col">
-						<input type="date" name="start_day" class="datepicker" value="<?php echo date('Y-m-d'); ?>" id="inputStartDay" v-model="start_date"/>
+					<div class="s12 m3 col">
+						<p>Incio</p>
+						<input class="datetimepiker" id="start" v-start>
 					</div>
 
-					<div class="s12 m4 col">
-						<input type="time" name="start_hour" value="<?php echo date('H:i'); ?>" id="inputStartHour" v-model="start_hour"/>
+					<div class="s12 m3 col">
+						<p>Fin</p>
+						<input class="datetimepiker" id="end" v-end>
 					</div>
 
-					<div class="s12 m4 col">
+					<div class="s12 m2 col">
 						<label>¿Todo el día?</label>
 						<div class="switch">
 							<label>
@@ -45,26 +47,6 @@
 
 			</div>
 
-			<div class="col s12 amber lighten-5 diverblock">
-
-				<span><strong>Fin</strong></span>
-
-				<div class="row">
-
-					<div class="s12 m6 col">
-						<div class="input-field">
-							<input type="date" name="end_day" class="datepicker" value="<?php echo \Carbon\Carbon::now()->format('Y-m-d'); ?>" id="inputEndDay" v-model="end_date"/>
-						</div>
-					</div>
-
-					<div class="s12 m6 col">
-						<div class="input-field">
-							<input type="time" name="end_hour" value="<?php echo \Carbon\Carbon::now()->addHours(2)->format('H:i'); ?>" id="inputEndHour" v-model="end_hour"/>
-						</div>
-					</div>
-
-				</div>
-			</div>
 
 			<div class="col s12 amber lighten-5 diverblock">
 				<div class="input-field">
@@ -75,7 +57,7 @@
 
 			<div class="col s12">
 				<div class="modal-footer">
-					<a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat" v-on="click: clearForm">Cancelar</a>
+					<a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Cancelar</a>
 					<button class="btn waves-effect waves-light" type="submit" name="action" id="myBoton">Crear
 						<i class="material-icons">send</i>
 					</button>
