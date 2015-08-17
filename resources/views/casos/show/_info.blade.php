cb95a67cdbe66cbc35e7a8842e2c207daa00b6a9
gbelot2003 <gbelot2003@hotmail.com>
2015-08-17 17:20:40 -0600

    cambios en diferentes archivos!

--- resources/views/casos/show/_info.blade.php
+++ resources/views/casos/show/_info.blade.php
@@ -2,25 +2,30 @@
 	<div class="col m3 s12">
 		<div class="row">
 			<div class="col m11 fheader">Cliente :</div>
-			<div class="col m11 purple lighten-5"><strong><a href="{{ url('/clientes', $caso->clientes->slug) }}">{{ $caso->clientes->name }}</a></strong></div>
+			<div class="col m11 blue lighten-5 showInfo"><strong><a href="{{ url('/clientes', $caso->clientes->slug) }}">{{ $caso->clientes->name }}</a></strong></div>
 		</div>
 	</div>
 
 	<div class="col m3 s12">
 		<div class="row">
 			<div class="col m11 fheader">Tribunal :</div>
-			<div class="col m11 purple lighten-5"><strong>{{ $caso->tribunal }}</strong></div>
+			<div class="col m11 blue lighten-5 showInfo"><strong>{{ $caso->tribunal }}</strong></div>
 		</div>
 	</div>
 
 	<div class="col m3 s12">
 		<div class="row">
 			<div class="col m11 fheader">Tipo :</div>
-			<div class="col m11 purple lighten-5"><strong>{{ $caso->tipocasos->name }} : {{ $caso->tipojuicio }}</strong></div>
+			<div class="col m11 blue lighten-5 showInfo"><strong>{{ $caso->tipocasos->name }} : {{ $caso->tipojuicio }}</strong></div>
 		</div>
 	</div>
 
-
+	<div class="col m3 s12">
+		<div class="row">
+			<div class="col m11 fheader">Juez :</div>
+			<div class="col m11 blue lighten-5 showInfo"><strong>{{ $caso->jueces->name }}</strong></div>
+		</div>
+	</div>
 
 </div>
 
@@ -28,14 +33,14 @@
 	<div class="col m6 s12">
 		<div class="row">
 			<div class="col m11 fheader">Demandante :</div>
-			<div class="col m11 purple lighten-5"><strong>{{ $caso->demandante }}</strong></div>
+			<div class="col m11 blue lighten-5 showInfo"><strong>{{ $caso->demandante }}</strong></div>
 		</div>
 	</div>
 
 	<div class="col m6 s12">
 		<div class="row">
 			<div class="col m11 fheader">Demandado :</div>
-			<div class="col m11 purple lighten-5"><strong>{{ $caso->demandado }}</strong></div>
+			<div class="col m11 blue lighten-5 showInfo"><strong>{{ $caso->demandado }}</strong></div>
 		</div>
 
 	</div>
