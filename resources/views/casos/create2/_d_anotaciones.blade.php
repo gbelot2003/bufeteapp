7d13ffe96224b4a23c1fa1c7da0904e0208ccd4d
gbelot2003 <gbelot2003@hotmail.com>
2015-08-27 20:32:37 -0600

    View, casos.create agregando _d_anotaciones

---
+++ resources/views/casos/create2/_d_anotaciones.blade.php
@@ -0,0 +1,6 @@
+<div class="row" xmlns="http://www.w3.org/1999/html">
+	<div class="input-field col s12">
+		<textarea id="casos.descripcion" type="text" class="materialize-textarea" v-model="caso.descripcion"/></textarea>
+		<label for="casos.title">Titulo</label>
+	</div>
+</div>
