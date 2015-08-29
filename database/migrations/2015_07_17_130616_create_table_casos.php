c8d738b73c31eabbbbd41051e4135962d635f063
gbelot2003 <gbelot2003@hotmail.com>
2015-08-29 12:29:09 -0600

    Agregando tabla pivote casos_contacto

--- database/migrations/2015_07_17_130616_create_table_casos.php
+++ database/migrations/2015_07_17_130616_create_table_casos.php
@@ -31,6 +31,15 @@ class CreateTableCasos extends Migration
 			$table->string('slug', 255);
 			$table->timestamps();
         });
+
+		Schema::create('casos_contacto', function(Blueprint $table){
+			$table->integer('casos_id')->unsigned()->index();
+			$table->foreign('casos_id')->references('id')->on('casos')->onDelete('cascade');
+
+			$table->integer('contacto_id')->unsigned()->index();
+			$table->foreign('contacto_id')->references('id')->on('contactos');
+			$table->timestamps();
+		});
     }
 
     /**
@@ -41,6 +50,8 @@ class CreateTableCasos extends Migration
     public function down()
     {
         Schema::table('casos', function (Blueprint $table) {
+
+			Schema::drop('casos_contacto');
 			Schema::drop('casos');
 		});
     }
