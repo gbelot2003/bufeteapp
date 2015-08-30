3cb548c695e01094237d51fcc06de1895dd62606
gbelot2003 <gbelot2003@hotmail.com>
2015-08-30 09:24:01 -0600

    actualizando sistema de Casos, agregando nuevas tablas y relaciones, configurando los modelos respectivos

--- database/migrations/2015_07_17_130616_create_table_casos.php
+++ database/migrations/2015_07_17_130616_create_table_casos.php
@@ -16,8 +16,10 @@ class CreateTableCasos extends Migration
 			$table->increments('id');
 			$table->string('caso');
 			$table->string('cliente_id');
+
 			$table->string('demandado')->nullable();
 			$table->string('demandante')->nullable();
+
 			$table->integer('tipocaso_id');
 			$table->string('tipojuicio');
 			$table->integer('tribunal_id');
@@ -32,13 +34,39 @@ class CreateTableCasos extends Migration
 			$table->timestamps();
         });
 
-		Schema::create('casos_contacto', function(Blueprint $table){
+		Schema::create('casos_demandados', function(Blueprint $table){
 			$table->integer('casos_id')->unsigned()->index();
 			$table->foreign('casos_id')->references('id')->on('casos')->onDelete('cascade');
 
 			$table->integer('contacto_id')->unsigned()->index();
 			$table->foreign('contacto_id')->references('id')->on('contactos');
 			$table->timestamps();
+		});
+
+		Schema::create('casos_demandates', function(Blueprint $table){
+			$table->integer('casos_id')->unsigned()->index();
+			$table->foreign('casos_id')->references('id')->on('casos')->onDelete('cascade');
+
+			$table->integer('contacto_id')->unsigned()->index();
+			$table->foreign('contacto_id')->references('id')->on('contactos');
+			$table->timestamps();
+		});
+
+		Schema::create('casos_tercerias', function(Blueprint $table){
+			$table->integer('casos_id')->unsigned()->index();
+			$table->foreign('casos_id')->references('id')->on('casos')->onDelete('cascade');
+
+			$table->integer('contacto_id')->unsigned()->index();
+			$table->foreign('contacto_id')->references('id')->on('contactos');
+
+			$table->integer('tipoterceria_id')->unsigned();
+
+			$table->timestamps();
+		});
+
+		Schema::create('tipo_tercerias', function(Blueprint $table){
+			$table->increments('id');
+			$table->string('name', 30);
 		});
     }
 
@@ -51,7 +79,10 @@ class CreateTableCasos extends Migration
     {
         Schema::table('casos', function (Blueprint $table) {
 
-			Schema::drop('casos_contacto');
+			Schema::drop('casos_tercerias');
+			Schema::drop('casos_demandados');
+			Schema::drop('casos_demandates');
+			Schema::drop('tipo_tercerias');
 			Schema::drop('casos');
 		});
     }
