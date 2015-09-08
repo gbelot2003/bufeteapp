5a6f778249e2a2614f8c2fc86859236f6b1beb1a
gbelot2003 <gbelot2003@hotmail.com>
2015-09-07 20:59:56 -0600

    Pruebas menores de envio de correo

--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'somegay@shomehost.com', 'name' => 'Some gay some host'],
 
     /*
     |--------------------------------------------------------------------------
