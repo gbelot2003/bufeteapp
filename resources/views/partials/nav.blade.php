729d7e2a5a722667432c55d446acaa2be12d09fd
Gerardo Belot <gerardobelot@develhn.com>
2016-05-13 18:24:59 -0500

    Update nav.blade.php

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,5 +1,5 @@
 <nav class="indigo darken-1">
-	<a class="" href="{{ url('/') }}"><h3>Sistema de Control de Casos</h3></a>
+	<a class="" href="{{ url('/') }}"><strong>Sistema de Control de Casos</strong></a>
 	<ul id="slide-out" class="side-nav">
 
 		@if (Auth::guest())
